81e11d62655a1e923e5499d4fcd8f9d456770300
Damian Leporis <38256909+dleporis@users.noreply.github.com>
2020-12-07 20:34:56 +0100

    Update server.py
    Still not pretty printing to the terminal, but works
    TODO: logging
    TODO: adjust the IP adress

--- Server/server.py
+++ Server/server.py
@@ -7,9 +7,9 @@ def xml_string_writer(time_stamp, station_id, rfid, estim_process_time):
     # make xml
     root = etree.Element("server_response")
 
-    time_stamp = etree.Element("time_stamp")
-    root.append(time_stamp)
-    time_stamp.text = f"{time_stamp}"
+    time_stamp_elem = etree.Element("time_stamp")
+    root.append(time_stamp_elem)
+    time_stamp_elem.text = f"{time_stamp}"
 
     station = etree.Element("station")
     root.append(station)
@@ -33,8 +33,25 @@ def xml_string_writer(time_stamp, station_id, rfid, estim_process_time):
     elif rfid > 5:
         side = "straight"
 
-    make_xml_cmd("wait", wait_time)
-    make_xml_cmd("junction", side)
+    def make_xml_cmd(command_type, value):
+        command_elem = etree.Element("command")
+        root.append(command_elem)
+        cmd_type_elem = etree.SubElement(command_elem, "cmd")
+        cmd_type_elem.text = f"{command_type}"
+        cmd_value_elem = etree.SubElement(command_elem, "val")
+        cmd_value_elem.text = f"{value}"
+    wait_cmd_str = "wait"
+    junction_cmd_str = "junction"
+    make_xml_cmd(wait_cmd_str, wait_time)
+    make_xml_cmd(junction_cmd_str, side)
+    print('{:-^70}'.format('Send info summary'))
+    print(f"Station ID: {station_id}\t\tRFID: {rfid}\t\tCommands sent at: {time_stamp}\t\tEstimated processing time: {estim_process_time}")
+    print('{:-^70}'.format('Send commands summary'))
+    print(f"Command: {wait_cmd_str} Value: {wait_time}\t\tCommand: {junction_cmd_str} Value: {side}")
+
+    # add to the log
+    # new line in log
+
     #indent_xml(root)
 
     tree = etree.ElementTree(root)
@@ -68,7 +85,7 @@ def xml_string_parser(recieved_xml_string):
         tree = etree.ElementTree(etree.fromstring(recieved_xml_string))
         root = tree.getroot()
         children = root.getchildren()
-        print(children)
+        # print(children) # prints out the list of the root children
 
         for time_stamp_elem in root.iter("time_stamp"):
             carrier_arrival_time_stamp = time_stamp_elem.text
@@ -80,16 +97,8 @@ def xml_string_parser(recieved_xml_string):
         for carrier_elem in root.iter("carrier"):
             for rfid_elem in carrier_elem.iter("RFID"):
                 rfid_recieved = rfid_elem.text
-        return carrier_arrival_time_stamp, station_id_recieved, rfid_recieved
+        return float(carrier_arrival_time_stamp), int(station_id_recieved), int(rfid_recieved)
 
-
-def make_xml_cmd(command_type, value):
-    command_elem = etree.Element("command")
-    root.append(command_elem)
-    cmd_type_elem = etree.SubElement(command_elem, "cmd")
-    cmd_type_elem.text = f"{command_type}"
-    cmd_value_elem = etree.SubElement(command_elem, "val")
-    cmd_value_elem.text = f"{value}"
 
 def indent_xml(elem, level=0):
     i = "\n" + level*"  "
@@ -120,54 +129,62 @@ if __name__ == "__main__":
     # arbitrary variables just for testing
     station_id_recv = 12
     rfid_recv = 8
-    estimated_process_time = 52680836 #ptf.get_est_proc_time(rfid, station_id)
+    
 
     while True:
         clientsocket, address = s.accept()
         # now our endpoint knows about the OTHER endpoint.
         print(f"Connection from {address} has been established.")
 
+        '''
         msg = "Welcome to the server!"
         msg = f"{len(msg):<{HEADERSIZE}}"+msg
 
         clientsocket.send(bytes(msg,"utf-8"))
-
+        '''
 
         full_recieved_from_client = ''
         new_msg = True
         while True:
-            msg = s.recv(16)
+            msg = clientsocket.recv(16)
 
             if new_msg:
-                print("new msg len:",msg[:HEADERSIZE])
+                # print("Message length:",msg[:HEADERSIZE])
                 msglen = int(msg[:HEADERSIZE])
                 new_msg = False
 
-            print(f"full message length: {msglen}")
+            # print(f"full message length: {msglen}")
 
             full_recieved_from_client += msg.decode("utf-8")
 
-            print(len(full_recieved_from_client))
+            # print(len(full_recieved_from_client))
 
 
             if len(full_recieved_from_client)-HEADERSIZE == msglen:
-                print("Full client message recieved")
-                print(full_recieved_from_client[HEADERSIZE:])
+                print("\n*********************************************************************")
+                print('{:*^70}'.format('Full client message recieved'))
+
                 recieved_from_client_cleaned = full_recieved_from_client [HEADERSIZE:]
+                print(f"Message length: {msglen}")
+                print("Full msg recieved:")
+                print(recieved_from_client_cleaned)
                 carrier_arr_time_stamp, station_id_recv, rfid_recv = xml_string_parser(recieved_from_client_cleaned)
                 
-                '{:*^30}'.format('Carrier arrival') # use '*' as a fill char
-                
-                print(f"Station ID: {station_id_recv}\tRFID: {rfid_recv}\tArrived at: {station_id_recv}\tEstimated processing time: {estimated_process_time}")
+                print('{:-^70}'.format('Carrier arrival summary'))
+                print(f"Station ID: {station_id_recv}\t\tRFID: {rfid_recv}\t\tArrived at: {carrier_arr_time_stamp}")
+
+                # get estimated process time
+                estimated_process_time = 112233445566 #ptf.get_est_proc_time(rfid, station_id)
                 # save to log here
                 
                 # write server response
                 server_time_stamp = time.time()
-                server_response_xml_string = xml_string_writer(time_stamp, station_id_recieved, rfid_recieved, estimated_process_time)
+                server_response_xml_string = xml_string_writer(server_time_stamp, station_id_recv, rfid_recv, estimated_process_time)
+                print('{:-^70}'.format('Send response message to the client'))
+                print(server_response_xml_string)
                 msg = f'{len(server_response_xml_string):<{HEADERSIZE}}'+server_response_xml_string # check f-string formatting
-                print("Send HEADER + message:")
-                print(msg)
                 clientsocket.send(bytes(msg, "utf-8"))
+                print("Waiting for client requests...")
 
                 new_msg = True
                 full_recieved_from_client = ""
