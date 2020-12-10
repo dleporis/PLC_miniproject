50a72776b3f21862b81a6f6dd0291232cdbca93b
AndreiVoica <45489065+AndreiVoica@users.noreply.github.com>
2020-12-10 17:16:23 +0100

    Update simple_server.py

--- Server/simple_server.py
+++ Server/simple_server.py
@@ -8,29 +8,31 @@ import time
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to the port
-server_address = ('172.20.66.78', 1234)
-print('starting up on %s port %s' % server_address)
-sock.bind(server_address)
+#server_address = ('172.20.66.48', 65432)
+sock.bind((socket.gethostname(), 65431))
+sock.listen(1)
+clientsocket, address = sock.accept()
+print(f"Connection from {address} has been established.")
 
-#give the station number
+# give the station number
 print("station:")
-station = int(input())
-
+station = int(input(10))
 
 # Wait for a connection and connect
-sock.listen(1)
-connection, client_address = sock.accept()
+#sock.listen(1)
+#connection, client_address = sock.accept()
 print('connection from', client_address)
 
 # Receive the data
 while True:
     data = connection.recv(64)
-    data1=data.decode('utf-8')
-            
+    data1 = data.decode('utf-8')
+
     if data:
         print('received: ' + data1)
-                
-        #make an xml file from the input
+
+        # make an xml file from the input
         f = open("xmldata.xml", "w")
         f.write(data1)
         f.close()
+
